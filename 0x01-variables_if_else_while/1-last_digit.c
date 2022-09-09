4e0114306e649b0a57433bc13ec37bb2c4e161b2
JuliusElvis <juliuselvis99@gmail.com>
2022-09-09 04:36:57 -0700

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,14 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ * int n - random number
+ *
+ * program to check
+ * section header
+ * Return: always 0
+ */
 int main(void)
 {
 int n;
