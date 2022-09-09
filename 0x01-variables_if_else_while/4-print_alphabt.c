b8d1898e4438b738909af8ecd2cf3bcc40641656
JuliusElvis <juliuselvis99@gmail.com>
2022-09-09 05:08:55 -0700

    commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - entry point
+ * char c
+ *
+ * program to print
+ * section header
+ * Return: always 0
+ */
 int main(void)
 {
 char c;
@@ -7,13 +14,12 @@ char c;
 for (c = 'a'; c <= 'z'; ++c)
 {
 	if (c == 'q' || c == 'e')
-{
-
+	{}
+	else
+	{
+		putchar(c);
+	}
 }
-else
-{
-putchar(c);
-}
-}
+putchar('\n');
 return (0);
 }
