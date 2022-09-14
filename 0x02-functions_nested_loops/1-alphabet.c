d4031565de0c6130c0c1289b6a289abd35dfbbc3
JuliusElvis <juliuselvis99@gmail.com>
2022-09-14 05:21:49 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+putchar('\n');
+
+}
