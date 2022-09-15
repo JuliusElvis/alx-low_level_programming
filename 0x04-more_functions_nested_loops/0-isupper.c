1596f96f96d306e0ff75167a3f424d9eba17ff13
JuliusElvis <juliuselvis99@gmail.com>
2022-09-15 01:40:40 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - evaluate if char is uppercase
+ * @c: print
+ * Return: always 0
+ */
+
+int _isupper(char c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
