122bf0965bf9a4e5eed2844d48e6565d987df34e
JuliusElvis <juliuselvis99@gmail.com>
2022-09-15 04:41:13 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_diagonal - entry point
+ * @n: param
+ * Return: no return
+ */
+void print_diagonal(int n)
+{
+int a;
+int b;
+
+if (n > 0)
+{
+for (a = 0; a < n; a++)
+{
+for (b = 0; b < i; b++)
+{
+_putchar (' ');
+}
+_putchar (92);
+_putchar ('\n');
+}
+}
+else
+{
+_putchar ('\n');
+}
+}
